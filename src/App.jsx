9e2b3c7623d152b4b9d68f51422d2be988b54e9c
miriam isela flores antonio <isc18350307@gmail.com>
2022-03-16 16:16:42 -0600

    ejemplo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
-//ECMAscript 5
-var textoVar = "texto var 1";
-
-//ECMAscript 6
-let textoLet = "texto let 1";
-const textoConst = "texto const 1";
-
-textoVar = "texto var 2";
-textoLet = "texto let 2";
-// textoConst = "texto const 2";
-
-var textoVar = "texto var 3";
-// let textoLet = "texto let 3";
-// const textoConst = "texto const 3";
-
-if (true){
-var textoVar = 5;
-let textoLet = 10;
-//textoConst = 15;
-}
 
 const App = () => {
+  const miObjeto = {
+      nombre: "miriam isela ",
+      apPaterno: "flores",
+      apMaterno:"antonio",
+      edad:"21",
+  };
+  const atributo = "apPaterno";
 return (
-<div className="App">
-<h1>{textoVar}</h1>
-<h1>{textoLet}</h1>
-<h1>{textoConst}</h1>
+    <div className="App">
+    <h1>{`mi nombre es ${miObjeto.nombre} ${miObjeto.apPaterno} ${miObjeto.apMaterno} y tengo ${miObjeto.edad} años`}</h1>
+    <h1></h1>
 </div>
 );
-}
+
+
+};
 
 export default App;

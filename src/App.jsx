efe0ac4560eada1631ee5e879af5acae142eddc7
miriam isela flores antonio <isc18350307@gmail.com>
2022-03-17 16:43:38 -0600

    ejemplo miriam

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,49 @@
-
 const App = () => {
-  const miObjeto = {
-      nombre: "miriam isela ",
-      apPaterno: "flores",
-      apMaterno:"antonio",
-      edad:"21",
+  const persona = {
+  nombre: "miriam isela ",
+  apellido: "flores antonio",
+  edad: 22,
+  direccion:{
+  calle: "avenida carranza entre guerrero y mutualismos",
+  numExt: 30,
+  numint: "",
+  ciudad: "san juan bautista tuxtepec",
+  }
   };
-  const atributo = "apPaterno";
-return (
-    <div className="App">
-    <h1>{`mi nombre es ${miObjeto.nombre} ${miObjeto.apPaterno} ${miObjeto.apMaterno} y tengo ${miObjeto.edad} años`}</h1>
-    <h1></h1>
-</div>
-);
 
+  const contactoPersona = {
+  telefono: "287 1452484",
+  email: "isc18350307@gmail.com",
+  web: "www.thesimpsons.com",
+  };
 
-};
+  const infoPersona = {...persona,...contactoPersona, empresa: "Disney"};
+  infoPersona.apodo = "Bart";
 
-export default App;
+  infoPersona.telefono = "555 555 5555";
+
+  console.table(infoPersona);
+
+  const{edad, nombre, apellido}= persona;
+  const{calle, numExt, numint,ciudad,} = persona.direccion;
+  const humanos = ["ambrocio", "marina","francisco javier", "jorge axel "];
+  const[padre, madre, hermano1, hermano2]= humanos;
+  const mascotas = ["Santa's helper", "show ball"]
+  const familia =[...humanos,...mascotas, "Milhouse"]
+  console.log(familia) 
+  
+  return (
+  <div className="App">
+  <h1>{`Mi nombre es ${nombre} ${apellido} y tengo ${edad} años y vivo en la  ${calle} numero ${numExt},${ciudad}`}</h1>
+  <br />
+  {"Mi familia la conforman:"}
+  <ul>
+  <li>{`Mi padre: ${padre}`}</li>
+  <li>{`Mi madre: ${madre}`}</li>
+  <li>{`Mis hermanos: ${hermano1} y ${hermano2}`}</li>
+  </ul>
+  </div>
+  );
+  }
+  export default App;
+  
